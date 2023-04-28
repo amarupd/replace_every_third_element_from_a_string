9937de84e66afd9785c543380a726f8a75c4df36
AMAR DUTT UPADHYAY <61179952+amarupd@users.noreply.github.com>
2023-04-28 18:13:21 +0530

    Update replace_every_third_element_from_a_string.js

--- replace_every_third_element_from_a_string.js
+++ replace_every_third_element_from_a_string.js
@@ -1,30 +1,24 @@
 //🙌🙌 replace every third element from a string 🙌🙌
 
-
 function sentence(sen) {
-    let str1 = str.replaceAll(" ", "")   // replacing all spaces with nospace
-
-    let tocken = 'xYjUki1'               //tocken if u want to use
-
-    let str2 = str1 + tocken            // concating both the string
-
-    //console.log(str2);                  //output :hitheremyselfamarduttupadhyayxYjUki1
-
+    let str1 = str.replaceAll(" ", "")                                       // replacing all spaces with nospace
+    let tocken = 'xYjUki1'                                                   //tocken if u want to use
+    let str2 = str1 + tocken                                                 // concating both the string
+    //console.log(str2);                                                     //output :hitheremyselfamarduttupadhyayxYjUki1
     let newStr = ''                     
-
     for (let i = 0; i < str.length; i++) {
         if ((i + 1) % 4 == 0) {
-            newStr += '_'                      // it will replace character at every consecutive 4th place with underscore
+            newStr += '_'                                                     // it will replace character at every consecutive 4th place with underscore
         } else {
-            newStr += str2[i]                  // it will copy the character at given index
+            newStr += str2[i]                                                 // it will copy the character at given index
         }
     }
-    return newStr                               //return new string
+    return newStr                                                              //return new string
 }
 
-
 let str = "hi there my self amar dutt upadhyay"  
-console.log(sentence(str));                        //calling the function
+console.log(sentence(str));                                                       //calling the function
+
 
 
 //output: hit_ere_yse_fam_rdu_tup_dhy_yxY_Uki
